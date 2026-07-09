99f5b5982a9f4c93574fdcb4971cb816ced34a0a
wringit <rp6784959@gmail.com>
2026-07-08 19:09:50 -0500

    Got rid of infinite loop

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -7,27 +8,35 @@ using UnityEngine.UI;
 public class Conversation : MonoBehaviour
 {
     // Start is called before the first frame update
-    public DialogueBox[] dialogue;
+    public DialogueBox[] dialogue = new DialogueBox[0];
 
-
+    //private void Start()
+    //{
+    //    dialogue = new DialogueBox[0];
+    //}
 
     public void Execute() {
         // Set to cannot move 
         for (int i = 0; i < dialogue.Length; i++)
         {
             // Wait for dialogue to be inactive
-            while (true) 
-            {
-                if (!dialogue[i].active)
-                {
-                    break;
-                }
-            }
+            DialogueRenderer.Render(dialogue[i]);
+            //while (true) 
+            //{
+            //    if (!dialogue[i].active)
+            //    {
+            //        break;
+            //    }
+            //}
             StartCoroutine(Wait(dialogue[i].wait));
         }
         IEnumerator Wait (float delay) {
             yield return new WaitForSecondsRealtime(delay);
         }
         // set to can move
+    }
+    public void addDialogue(DialogueBox dialogueBox) { 
+        Array.Resize(ref dialogue, dialogue.Length+1);
+        dialogue[dialogue.Length-1] = dialogueBox;
     }
 }
