117549a6dbb71a4911ac0a60533ed66b2f6de1a1
Stuuupidfish <elainexyz2006@gmail.com>
2026-07-27 14:58:08 -0700

    day transition countdown (not tested yet)

--- Assets/Scripts/DaySystem/DayTransitionButtons.cs
+++ Assets/Scripts/DaySystem/DayTransitionButtons.cs
@@ -7,6 +7,8 @@ public class DayTransitionButtons : MonoBehaviour
 {
     [SerializeField] private string HOME_SCENE = "Home";
     [SerializeField] private string MENU_SCENE = "Menu";
+    [SerializeField] private DayCountdown dayCountdown;
+    [SerializeField] private float countdownDuration = 1f;
     public void SaveAndContinue()
     {
         Save();
@@ -25,11 +27,19 @@ public class DayTransitionButtons : MonoBehaviour
     public void Continue()
     {
         Debug.Log("Continuing...");
+        StartCoroutine(ShowCountdownAndContinue());
         SceneManager.LoadScene(HOME_SCENE); //NOT USING SCENE LOADER BECAUSE NO SCENE DATA DOES NEEDS TO BE SAVED ONTO GAMEDATA
     }
     private void Save()
     {
         Debug.Log("Saving...");
         DataPersistenceManager.Instance.SavePlayerData();//SAVE TO JSON
+    }
+    
+    private IEnumerator ShowCountdownAndContinue()
+    {
+        dayCountdown.ShowCountdown();
+        yield return new WaitForSeconds(countdownDuration);
+        Continue();
     }
 }
