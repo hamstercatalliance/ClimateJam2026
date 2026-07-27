2d687cfc8ad6684d9d04f3a9f6c6b82c32eb929f
wringit <rp6784959@gmail.com>
2026-07-26 21:56:20 -0500

    Fix dialogue active bool issue

--- Assets/Scripts/DialogueHandlers/EndDayEarlyHandler.cs
+++ Assets/Scripts/DialogueHandlers/EndDayEarlyHandler.cs
@@ -7,10 +7,11 @@ public class EndDayEarlyHandler : DialogueSignalHandler
 {
     protected override void HandleDialogueSignal(object sender, EventArgs e)
     {
-            //DialogueRenderer.Instance.dialogueSignal += (sender, e) => {
+        //DialogueRenderer.Instance.dialogueSignal += (sender, e) => {
             Debug.Log("DialogueRenderer: Received dialogue signal: " + e.ToString());
             if (e is DialogueSignal signal && signal.signal == "EndDayEarly")
             {
+                DialogueBox.dialogueActive = false;
                 DayManager.Instance.EndDay();
             }
             //};
