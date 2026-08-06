67ccf25e64d99110e061598f3d846d56ed2bb704
Gamer Man <grantyab@gmail.com>
2026-08-06 03:38:09 -0700

    downtownsceneinteriorandcamerawork

--- Assets/Scripts/EnvironmentFade/CameraCollision.cs
+++ Assets/Scripts/EnvironmentFade/CameraCollision.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CameraCollision : MonoBehaviour
 {
-
-    [SerializeField]
-    private bool fadeOut = true;
+    public enum CamAnimation
+    {
+        ZoomIn,
+        ZoomOut,
+        ResetZoomOut,
+        ResetZoomIn,
+        None
+    }
+    [SerializeField] private bool fadeOut = true;
     public GameObject toFade = null; //set this to an object with a fade script, make sure material is using transparent shadergraph
+    [SerializeField] private Animator cameraAnim = null;
+    [SerializeField] private bool cameraReset = false;
+    [SerializeField] CamAnimation SelectCamAnimation = CamAnimation.None;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,6 +32,27 @@ public class CameraCollision : MonoBehaviour
             {
                 toFade.GetComponent<Fade>().fadeNow = fadeOut;
             }
+            if(cameraAnim != null && !cameraReset)
+            {
+                switch (SelectCamAnimation)
+                {
+                    case CamAnimation.ZoomIn:
+                        cameraAnim.Play("ZoomIn");
+                        break;
+                    case CamAnimation.ZoomOut:
+                        cameraAnim.Play("ZoomOut");
+                        break;
+                    case CamAnimation.ResetZoomIn:
+                        cameraAnim.Play("ResetFromZoomIn");
+                        break;
+                    case CamAnimation.ResetZoomOut:
+                        cameraAnim.Play("ResetFromZoomOut");
+                        break;
+                    case CamAnimation.None:
+                        break;
+                }
+            }
+            cameraReset = !cameraReset;
         }
     }
 }
