58e67865d0e9a445c6f9c80df4cf0eb6fd43c99c
Stuuupidfish <elainexyz2006@gmail.com>
2026-08-06 22:01:33 -0500

    end day cutscene fixed, home scene starting to get modifies to keep consistent artstyle

--- Assets/Scripts/DaySystem/DataCenterCycling.cs
+++ Assets/Scripts/DaySystem/DataCenterCycling.cs
@@ -11,23 +11,22 @@ public class DataCenterCycling : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DataCenterModels[0].SetActive(false);
-        DataCenterModels[1].SetActive(false);
-        DataCenterModels[2].SetActive(false);
-        DataCenterModels[3].SetActive(false);
+        foreach (GameObject model in DataCenterModels)
+        {
+            model.SetActive(false);
+        }
         dayCount = DayManager.Instance.dayCount;
         DataCenterModels[dayCount].SetActive(true);
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(DayManager.Instance.dayCount != dayCount)
-        {
-           DataCenterModels[dayCount].SetActive(false);
-           dayCount = DayManager.Instance.dayCount;
-           DataCenterModels[dayCount].SetActive(true);
-        }
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if(DayManager.Instance.dayCount != dayCount)
+    //     {
+    //        DataCenterModels[dayCount].SetActive(false);
+    //        dayCount = DayManager.Instance.dayCount;
+    //        DataCenterModels[dayCount].SetActive(true);
+    //     }
+    // }
 }
