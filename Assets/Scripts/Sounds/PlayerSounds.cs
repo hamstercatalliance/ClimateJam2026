9c60f2629d606ed78326b06dd188cc1ba6910970
Stuuupidfish <elainexyz2006@gmail.com>
2026-08-08 15:55:41 -0500

    different walking sounds for different surface in downtown

--- Assets/Scripts/Sounds/PlayerSounds.cs
+++ Assets/Scripts/Sounds/PlayerSounds.cs
@@ -7,9 +7,21 @@ public class PlayerSounds : MonoBehaviour
     private Player player;
     private float footstepTimer;
     private float footstepTimerMax = 0.5f;
+    public enum SurfaceType
+    {
+        Grass,
+        Stone,
+        Wood,
+        Sand
+    }
+    private SurfaceType currentSurfaceType = SurfaceType.Stone;
     private void Awake()
     {
         player = GetComponent<Player>();
+    }
+    private void Start()
+    {
+        footstepTimer = footstepTimerMax;
     }
     private void Update()
     {
@@ -20,8 +32,27 @@ public class PlayerSounds : MonoBehaviour
             footstepTimer = footstepTimerMax;
             if (player.IsWalking && player.IsGrounded)
             {
-                SoundManager.Instance.PlayFootstepsSound(player.transform.position);
+                SoundManager.Instance.PlayFootstepsSound(player.transform.position, currentSurfaceType);
             }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grass"))
+        {
+            currentSurfaceType = SurfaceType.Grass;
+        }
+        else if (other.CompareTag("Stone"))
+        {
+            currentSurfaceType = SurfaceType.Stone;
+        }
+        else if (other.CompareTag("Wood"))
+        {
+            currentSurfaceType = SurfaceType.Wood;
+        }
+        else if (other.CompareTag("Sand"))
+        {
+            currentSurfaceType = SurfaceType.Sand;
         }
     }
 }
--- Assets/Scripts/Sounds/SoundManager.cs
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -97,12 +97,28 @@ public class SoundManager : MonoBehaviour, IHasPersistentData
         Player.Instance.OnPlayerJump -= Player_OnPlayerJump;
         DialogueRenderer.OnDialogueProceed -= DialogueRenderer_OnDialogueProceed;
     }
-    public void PlayFootstepsSound(Vector3 position)
+    public void PlayFootstepsSound(Vector3 position, PlayerSounds.SurfaceType surfaceType = PlayerSounds.SurfaceType.Stone)
     {
         AudioClip[] footstepSounds = null;
-        // Determine the surface type 
-
-        PlaySound(stoneWalkingSounds, position); //for now, just play grass walking sounds
+        switch (surfaceType)
+        {
+            case PlayerSounds.SurfaceType.Grass:
+                footstepSounds = grassWalkingSounds;
+                break;
+            case PlayerSounds.SurfaceType.Stone:
+                footstepSounds = stoneWalkingSounds;
+                break;
+            case PlayerSounds.SurfaceType.Wood:
+                footstepSounds = woodWalkingSounds;
+                break;
+            case PlayerSounds.SurfaceType.Sand:
+                footstepSounds = sandWalkingSounds;
+                break;
+            default:
+                footstepSounds = stoneWalkingSounds; // Default
+                break;
+        }
+        PlaySound(footstepSounds, position); 
     }
     #region Event Handlers
     private void DialogueRenderer_OnDialogueProceed(object sender, EventArgs e)
